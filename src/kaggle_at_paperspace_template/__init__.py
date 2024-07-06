223a8e29f0461c8323b1898b9f2d51aa44c15d49
sinchir0 <dekunattou@gmail.com>
2024-07-06 13:00:02 +0900

    add

---
+++ src/kaggle_at_paperspace_template/__init__.py
@@ -0,0 +1,3 @@
+def main() -> int:
+    print("Hello from kaggle-at-paperspace-template!")
+    return 0
---
+++ src/kaggle_at_paperspace_template/__main__.py
@@ -0,0 +1,4 @@
+import kaggle_at_paperspace_template
+import sys
+
+sys.exit(kaggle_at_paperspace_template.main())
